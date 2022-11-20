5702fe405adbe83e9497c21523bea303c67414b6
github username <github email address>
2022-11-20 16:24:11 -0500

    ADD: Articles object

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, :slug, :content, presence: true
+  validates :slug, uniqueness: true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    let(:article) { create(:article) }
+
+    it 'tests that factory is valid' do
+      expect(article).to be_valid
+    end
+
+    it 'expects article to have a title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it 'has invalid content' do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it 'has an invalid slug' do
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+
+    it 'validates the uniqueness of the slug' do
+      invalidArticle = build(:article, slug: article.slug)
+      expect(invalidArticle).not_to be_valid
+    end
+  end
 end
